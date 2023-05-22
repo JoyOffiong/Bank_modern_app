bcc42c05948092bbf16753f5e8c1081cf42ccd41
Joy Offiong <blaqkly@gmail.com>
2023-05-22 07:50:18 +0100

    another commit

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -8,7 +8,7 @@ const Clients = () => {
     {clients.map((client)=>{
         return(
             <div key={client.id} className={` flex-1 sm:min-w-[192px] flex  flex-wrap`}>
-                <img src={client.logo} alt="logo" className="sm:w-[192px] mr-[170px] mb-[20px] cursor-pointer hover:translate-y-1.5   w-[100px] object-contain" />
+                <img src={client.logo} alt="logo" className="sm:w-[192px] mr-[120px] mb-[20px] cursor-pointer hover:translate-y-1.5   w-[100px] object-contain" />
 
             </div>
         )
